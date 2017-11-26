c29ee667e5ad1e8a516ee8867ac5d8fe8cbf0e02
Mark Kudyarsky <31584290+Kudyarsky@users.noreply.github.com>
2017-11-26 19:46:22 +0200

    added files

---
+++ main.js
@@ -0,0 +1,28 @@
+<a href="#Portfolio" class="scrollto">Portfolio</a>
+
+<script type="text/javascript">
+    $(document).ready(function() {
+  $("a.scrollto").click(function() {
+    var elementClick = $(this).attr("href")
+    var destination = $(elementClick).offset().top;
+    jQuery("html:not(:animated),body:not(:animated)").animate({
+      scrollTop: destination
+    }, 1200);
+    return false;
+  });
+});
+  </script>
+<a href="#Contact​" class="scrollto">Contact​</a>
+
+<script type="text/javascript">
+    $(document).ready(function() {
+  $("a.scrollto").click(function() {
+    var elementClick = $(this).attr("href")
+    var destination = $(elementClick).offset().top;
+    jQuery("html:not(:animated),body:not(:animated)").animate({
+      scrollTop: destination
+    }, 1200);
+    return false;
+  });
+});
+  </script>
